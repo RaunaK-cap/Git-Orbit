fe3d1480db98033a237a099b63259171c86aeb93
Raunak <raunakkumar6437@gmail.com>
2025-09-05 00:06:00 +0530

    feat:clerk sign-in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,8 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-    return <SignIn />
+    return <div className='flex h-screen  justify-center items-center'>
+
+      <SignIn />
+      </div>
   }
