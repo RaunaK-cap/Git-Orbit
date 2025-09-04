0ed2cb956a9c6eed6895dcc4bd603f09497d5d53
Raunak <raunakkumar6437@gmail.com>
2025-09-05 00:06:27 +0530

    feat:clerk sign-up page

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,4 @@
 import { SignUp } from '@clerk/nextjs'
 export default function page(){
-    return <> <SignUp/> </>
-}
+    return <div className='flex h-screen items-center justify-center'> <SignUp/> </div>
+} 
