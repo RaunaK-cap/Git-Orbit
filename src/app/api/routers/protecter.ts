17e674a61cf267337e5baf870cc64003fb8211b0
Raunak <raunakkumar6437@gmail.com>
2025-09-11 13:55:35 +0530

    feat: added query

--- src/app/api/routers/protecter.ts
+++ src/app/api/routers/protecter.ts
@@ -1,22 +1,46 @@
-import { protectedRouter, publicProcedure , router } from "../trpc";
+import { protectedRouter, router } from "../trpc";
 import z from "zod"
 
 export const projectRouter = router({
     createProject: protectedRouter
-    .input(
-        z.object({
-            name:z.string(),
-            GithubURl: z.string(),
-            GithubToken: z.string().optional()
-        })
-    )
-    .mutation(async( { ctx , input})=>{
-        console.log("your input all values:" , input)
+        .input(
+            z.object({
+                name: z.string(),
+                GithubURl: z.string(),
+                GithubToken: z.string().optional()
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            // call db here save data using // ctx has session user id 
+            const projects = await ctx.Dbclient.project.create({
+                data: {
+                    GithubURl: input.GithubURl,
+                    RepoName: input.name,
+                    projects: {
+                        create: {
+                            userId: ctx.user.userId!
+                        }
+                    }
+                },
+            })
 
-        // call db here save data using // ctx has session user id 
-        return true
-    })
-     
+            return projects
+        }),
+
+        getproject: protectedRouter.query(async({ ctx })=>{
+            const projectdetail = await ctx.Dbclient.project.findMany({
+                where:{
+                    projects:{
+                        some:{
+                            userId:ctx.user.userId!
+                        }
+                    }
+                }
+            })
+            return projectdetail
+
+        })
+
 })
 
 
